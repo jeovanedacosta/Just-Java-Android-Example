1502ae0f8ef4c67170b0138f23f5490b00d2e50d
jeovane da costa <jeovanebc@gmail.com>
2017-07-15 17:47:01 -0300

    first commit

---
+++ app/src/main/java/com/jeovane/justjava/MainActivity.java
@@ -0,0 +1,67 @@
+package com.jeovane.justjava; /**
+ * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ *
+ * package com.example.android.justjava;
+ *
+ */
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.jeovane.justjava.R;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private int quantity = 0;
+    private int price = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        displayPrice(this.quantity * this.price);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void increment(View view) {
+        this.quantity++;
+        display(this.quantity);
+    }
+
+    public void decrement(View view) {
+        if (this.quantity > 0) {
+            this.quantity--;
+        }
+        display(this.quantity);
+    }
+}
